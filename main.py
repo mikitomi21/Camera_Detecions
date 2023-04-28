133a5b4f892e391d8b6dc8581f5d041f622da8d4
Kuba Stachowicz <kubastach2001@gmail.com>
2023-04-28 13:57:22 +0200

    Add taking a picture

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ import cv2
 # define a video capture object
 camera = cv2.VideoCapture(0)
 
+i=0
 while (True):
 
     # Capture the video frame
     # by frame
-    ret, frame = camera.read()
+    result, image = camera.read()
 
+    if i==0 and result:
+        cv2.imwrite("Test_photo.png", image)
     # Display the resulting frame
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', image)
 
     # the 'q' button is set as the
     # quitting button you may use any
     # desired button of your choice
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    i+=1
 
 # After the loop release the cap object
 camera.release()
