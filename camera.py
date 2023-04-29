e491b22f09a6b3b5d1bb9d065e86e6d92fb3e6ba
Kuba Stachowicz <kubastach2001@gmail.com>
2023-04-29 22:18:14 +0200

    Change size of including photo and add label value

--- camera.py
+++ camera.py
@@ -1,28 +1,20 @@
 import cv2
 
-# define a video capture object
 camera = cv2.VideoCapture(0)
 
 i=0
 while (True):
-
-    # Capture the video frame
-    # by frame
     result, image = camera.read()
 
-    if result:
-        cv2.imwrite(f"img/F_Humna{i}.png", image)
-    # Display the resulting frame
+    # if result:
+    #     cv2.imwrite(f"img/F_Humna{i}.png", image)
+
     cv2.imshow('frame', image)
 
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     i+=1
 
-# After the loop release the cap object
+
 camera.release()
-# Destroy all the windows
 cv2.destroyAllWindows()
--- img_manager.py
+++ img_manager.py
@@ -1,4 +1,7 @@
-from skimage.io import imread, imshow
+import skimage
+from skimage.transform import resize
+from skimage.io import imread
+import numpy as np
 import os
 
 def load_data(dir):
@@ -6,8 +9,15 @@ def load_data(dir):
     labels = []
     for file in os.listdir(dir):
         path = dir+file
-        images.append(imread(path))
+        img = imread(path)
+        resized_img = skimage.transform.resize(img, (80,80))
+        
+        images.append(resized_img)
 
         #TODO Think about labels later
-        labels.append(1)
+        if file[0] == 'T':
+            labels.append(1)
+        else:
+            labels.append(0)
+        
     return images, labels
