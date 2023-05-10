c885987568ce55a63e1088b9d2b01551ca3a1fbe
Kuba Stachowicz <kubastach2001@gmail.com>
2023-05-10 13:53:04 +0200

    Create Convolutional neural network and connect this with camera

--- camera.py
+++ camera.py
@@ -1,15 +1,49 @@
 import cv2
+import tensorflow as tf
+import numpy as np
+from keras.utils import load_img, img_to_array
+
+def draw_border(img, color, thickness=5):
+    height, width = img.shape[:2]
+    cv2.rectangle(img, (0, 0), (width, height), color, thickness)
+
+def check_img(path):
+    img = load_img(path, target_size=(200,200))
+
+    X = img_to_array(img)
+    X = np.expand_dims(X, axis = 0)
+    images = np.vstack([X])
+    label = model.predict(images)
+
+    if label == 0:
+            print("Not Human")
+            return 0
+    elif label == 1:
+            print("Human")
+            return 1
+    return 0
+
+model = tf.keras.models.load_model('model/face_reco.h5')
 
 camera = cv2.VideoCapture(0)
 
 i=0
+GREEN = (0,255,0)
+RED = (0,0,255)
 while (True):
-    result, image = camera.read()
+    result, img = camera.read()
 
-    # if result:
-    #     cv2.imwrite(f"img/F_Humna{i}.png", image)
+    if result:
+        path = "img/Current_Camera.png"
+        cv2.imwrite(path, img)
+        label = check_img(path)
+        if label:  
+            draw_border(img, GREEN, 20)
+        else:
+            draw_border(img, RED, 20)
+             
 
-    cv2.imshow('frame', image)
+    cv2.imshow('frame', img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
