70e2f795df981797aa1de30fb6827c71840477c3
mikitomi21 <kubastach2001@gmail.com>
2023-09-26 18:41:26 +0200

    Change the way of uploading images

--- img_manager.py
+++ img_manager.py
@@ -1,31 +1,41 @@
-from skimage import transform
-from skimage.io import imread
-from skimage.color import rgb2gray
-import numpy as np
 import os
+import cv2
+import tensorflow as tf
 
-X_SIZE = 26
-Y_SIZE = 26
+def shuffle(imgs: tf.Tensor, labels:tf.Tensor) -> tuple[list:tf.Tensor, list:tf.Tensor]:
+    idx = tf.range(tf.shape(labels)[0])
+    shuffled_idx = tf.random.shuffle(idx)
+    imgs_tmp = tf.gather(imgs, shuffled_idx)
+    labels_tmp = tf.gather(labels, shuffled_idx)
+    return imgs_tmp, labels_tmp
 
-def load_data(dir):
-    images = []
-    labels = []
-    for file in os.listdir(dir):
-        path = dir+file
-        img = imread(path)
+def load_data() -> tuple[tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor]]:
+    main_paths = ['img/train/', 'img/validation/']
+    size = (26,26)
+    is_human = False
+    i = 0
+    for main_path in main_paths:
+        imgs = []
+        labels = []
+        for dir in os.listdir(main_path):
+            if dir[0] == 'T':
+                is_human = True
+            elif dir[0] == 'F':
+                is_human = False
+            path = main_path + dir + '/'
 
-        # Change a size
-        img = transform.resize(img, (Y_SIZE, X_SIZE))
+            for file in os.listdir(path):
+                img = cv2.imread(path+file)
+                img = cv2.resize(img, size)
+                img = img.astype(dtype="float32") / 255.0
+                imgs.append(img)
+                labels.append(is_human)
+                i+=1
+                # print(i)
+                if i % 1000 == 0 and i > 0:
+                    print(f"Uploaded {i} photos")
 
-        # Change a color
-        img = rgb2gray(img)
+        imgs, labels = tf.constant(imgs), tf.constant(labels)
+        imgs, labels = shuffle(imgs, labels)
 
-        images.append(img)
-
-        #TODO Think about labels later
-        if file[0] == 'T':
-            labels.append(1)
-        else:
-            labels.append(0)
-        
-    return images, labels
+        yield (imgs, labels)
